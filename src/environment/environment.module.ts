51bd73ee546725171831fd0947747b92c65d021b
최강훈 <ganghun@lepisode.team>
2022-10-02 15:14:23 +0900

    chore: updated .prettier config file

--- src/environment/environment.module.ts
+++ src/environment/environment.module.ts
@@ -5,32 +5,27 @@ import * as fs from 'fs';
 import { EnvironmentService } from './environment.service';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      envFilePath: path.join(__dirname, '..', `.env.${process.env.NODE_ENV}`),
-      isGlobal: true,
-    }),
-  ],
-  providers: [EnvironmentService],
+	imports: [
+		ConfigModule.forRoot({
+			envFilePath: path.join(__dirname, '..', `.env.${process.env.NODE_ENV}`),
+			isGlobal: true,
+		}),
+	],
+	providers: [EnvironmentService],
 })
 export class EnvironmentModule implements OnModuleInit {
-  /**
-   * ENV 파일 존재 여부를 확인합니다.
-   * @author 최강훈 <ganghun@lepisode.team>
-   * @since 1.0.0
-   */
-  onModuleInit() {
-    const envFilePath = path.join(
-      __dirname,
-      '..',
-      '..',
-      `.env.${process.env.NODE_ENV}`,
-    );
-    console.log(envFilePath);
-    try {
-      fs.readFileSync(envFilePath);
-    } catch (e) {
-      throw new Error('ENV 파일을 찾을 수 없습니다.');
-    }
-  }
+	/**
+	 * ENV 파일 존재 여부를 확인합니다.
+	 * @author 최강훈 <ganghun@lepisode.team>
+	 * @since 1.0.0
+	 */
+	onModuleInit() {
+		const envFilePath = path.join(__dirname, '..', '..', `.env.${process.env.NODE_ENV}`);
+
+		try {
+			fs.readFileSync(envFilePath);
+		} catch (e) {
+			throw new Error('ENV 파일을 찾을 수 없습니다.');
+		}
+	}
 }
