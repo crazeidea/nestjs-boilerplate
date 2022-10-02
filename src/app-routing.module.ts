5f9a29c08b064a3e61e85536a6a8cda428abf0ec
최강훈 <crazeidea@gmail.com>
2022-10-02 16:01:28 +0900

    feat: added prisma client

--- src/app-routing.module.ts
+++ src/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { RouterModule, Routes } from '@nestjs/core';
+import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 const routes: Routes = [];
 
 @Module({
-	imports: [RouterModule.register(routes)],
+	imports: [RouterModule.register(routes), PrismaModule],
 	exports: [RouterModule],
+	providers: [PrismaService],
 })
 export class AppRoutingModule {}
